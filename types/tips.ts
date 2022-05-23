0021c3d7d1e42ad648fbbec49e33cc156413c2e2
Steven Hall <steven.h@ansicgroup.com>
2022-05-23 17:33:19 +1000

    wip

---
+++ types/tips.ts
@@ -0,0 +1,11 @@
+export interface Tip {
+    RaceID: String;
+    formula: 1 | 2 | 3 | 4 | 5 | 6 | 7 |8;
+    method: 1 | 2| 3;
+    nagoshi: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |8;
+    multi: 0 | 1;
+    First: String;
+    Second: string;
+    Third: string;
+    amount: number;
+}
