0b1f2adea8e68050132e613f4b037f0d57417da8
Steven Hall <steven.h@ansicgroup.com>
2022-05-24 14:48:50 +1000

    delete fixed

--- pages/api/deleteDatabaseEntries.ts
+++ pages/api/deleteDatabaseEntries.ts
@@ -4,23 +4,37 @@ import awsExports from "../../src/aws-exports";
 import AWS from "aws-sdk";
 
 AWS.config.update({
-    region: awsExports.aws_project_region,
+  region: awsExports.aws_project_region,
 });
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-
-    const dynamo = new AWS.DynamoDB();
-    const results = []
-    const partiqlStmnt = {
-        Statement: `DELETE * FROM "umanitydb-${process.env.USER_BRANCH}"`,
-    };
     try {
-        const response = await dynamo.executeStatement(partiqlStmnt).promise();        
-        res.status(201).json({});
+      const dynamo = new AWS.DynamoDB();
+      const results: Tip[] = [];
+      const partiqlStmnt = {
+        Statement: `SELECT * FROM "umanitydb-${process.env.USER_BRANCH}"`,
+      };
+      const response = await dynamo.executeStatement(partiqlStmnt).promise();
+      response.Items?.forEach((item) => {
+        const row = AWS.DynamoDB.Converter.unmarshall(item) as Tip;
+
+        results.push(row);
+      });
+
+      for (let x = 0; x < results.length; x++) {
+        const partiqlStmnt = {
+          Statement: `DELETE FROM "umanitydb-${process.env.USER_BRANCH}" WHERE "UUID" = ${results[x].UUID}`,
+        };
+        const result = await dynamo.executeStatement(partiqlStmnt).promise();
+      }
+
+      res.status(201).json({results});
     } catch (e) {
-        res.status(500).json(e);
+      const result = JSON.stringify((e as Error).message);
+      res.status(500).json(result);
     }
+
 }
