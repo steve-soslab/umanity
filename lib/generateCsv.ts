82f888ac8f035d89f72bacb32f9d14d42d528e13
ryanlabsglobal <ryan.s@soslab.global>
2022-05-25 16:47:22 +1000

    fixed type

--- lib/generateCsv.ts
+++ lib/generateCsv.ts
@@ -1,6 +1,6 @@
 import { Tip } from "../types/tips";
 const generateCsv = (tip: Tip) => {
-  return `${tip.RaceID},${tip.formula},${tip.method},${tip.nagoshi},${tip.multi},${tip.First},${tip.Second},${tip.Third},${tip.amount}\n`;
+  return `${tip.RaceID},${tip.formula},${tip.method},${tip.banker},${tip.multi},${tip.First},${tip.Second},${tip.Third},${tip.amount}\n`;
 };
 
 export default generateCsv;
